790bd454d5729673bdf2a28dfefd24bc7c54fe20
mhfvuong <mitchellvuong@hotmail.com>
2022-12-03 11:26:32 +0100

    can choose between real and test data using user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::path::PathBuf;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut user_input = String::new();
+    let stdin = io::stdin();
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("src");
+    stdin.read_line(&mut user_input);
+    if user_input.trim() == String::from("test") {
+        path.push("test_input.txt");
+        println!("test input:");
+    }
+    else {
+        path.push("input.txt");
+        println!("real data input:");
+    }
+
+    // path.push("")
 }
