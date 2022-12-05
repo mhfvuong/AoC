380637b9ad1f43c2cfdcea15fc9fe44892689b70
mhfvuong <mitchellvuong@hotmail.com>
2022-12-05 08:23:10 +0100

    making template day 5

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,3 +1,16 @@
-pub fn crate_crane(data_string: String) {
+use std::collections::HashMap;
 
+pub fn crate_crane(data_string: String) {
+    let lines = data_string.lines();
+    let mut instruction:bool = false;
+    let crates: HashMap<usize, Vec<&str>> = HashMap::new();
+    for line in lines {
+        if line.len() > 1 && !instruction{
+            todo!() // read data of the crates
+        }
+        else if line.len() > 1 && instruction{
+            todo!() // read instructions
+        }
+        if line == "" {instruction = true;}
+    }
 }
