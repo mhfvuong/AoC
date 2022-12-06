eb5ea6aa85bb42b34a50738393e0afaa1b3c10df
mhfvuong <mitchellvuong@hotmail.com>
2022-12-06 12:38:57 +0100

    part 2 day 6

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -6,17 +6,15 @@ pub fn tune_device(data_string: String) {
     let mut marker = Vec::new();
     let mut marker_found: bool = false;
     let mut marker_check = Vec::new();
-    let mut n = 0;
     for line in lines {
         for letter in line.chars() {
             marker.push(letter);
-            if marker.len() >= 4 {
+            if marker.len() >= 14 {
                 marker_found = true;
-                for i in 1..=4 {
+                for i in 1..=14 {
                     if marker_check.contains(&marker[marker.len() - i]) {
                         marker_found = false;
                     } else { marker_check.push(marker[marker.len() - i]) }
-
                 }
             }
             marker_check.drain(..);
