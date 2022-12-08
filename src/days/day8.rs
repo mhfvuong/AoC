2bba480990ee602cf0a0f385583e598b97737097
mhfvuong <mitchellvuong@hotmail.com>
2022-12-08 11:53:03 +0100

    day 8 complete

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -12,56 +12,33 @@ pub fn visible_trees(data_string: String) {
         }
         i += 1;
     }
-
     let mut visible_trees = 0;
     let mut highest_scenic_score = 0;
     for i in 0..grid.len() {
         for j in 0..grid[0].len() {
-            // let _current_tree = grid[i][j];
-            let mut score_x = [1;2];
-            let mut score_y = [1;2];
+            let mut score_x = [0;2];
+            let mut score_y = [0;2];
             let mut taller_x = [false; 2];
             let mut taller_y = [false; 2];
-            // println!("coordinate: {}, {}", &i, &j);
             for x in j+1..grid.len(){
-                if grid[i][x] >= grid[i][j] {
-                    // println!("x0: {} is taller than {}", &grid[i][x], &grid[i][j]);
-                    taller_x[0] = true;
-                    if !taller_x[0] {
-                        score_x[0] += 1;
-                    }
-                }
+                if !taller_x[0] { score_x[0] += 1; }
+                if grid[i][x] >= grid[i][j] { taller_x[0] = true; }
             }
             let mut x = j;
             while x > 0 {
                 x -= 1;
-                if grid[i][x] >= grid[i][j] {
-                    // println!("x1: {} is taller than {}", &grid[i][x], &grid[i][j]);
-                    taller_x[1] = true;
-                    if !taller_x[1] {
-                        score_x[1] += 1;
-                    }
-                }
+                if !taller_x[1] { score_x[1] += 1; }
+                if grid[i][x] >= grid[i][j] { taller_x[1] = true; }
             }
             for y in i+1..grid.len(){
-                if grid[y][j] >= grid[i][j] {
-                    // println!("y0: {} is taller than {}", &grid[y][j], &grid[i][j]);
-                    taller_y[0] = true;
-                    if !taller_y[0] {
-                        score_y[0] += 1;
-                    }
-                }
+                if !taller_y[0] { score_y[0] += 1; }
+                if grid[y][j] >= grid[i][j] { taller_y[0] = true; }
             }
             let mut y = i;
             while y > 0 {
                 y -= 1;
-                if grid[y][j] >= grid[i][j] {
-                    // println!("y1: {} is taller than {}", &grid[y][j], &grid[i][j]);
-                    taller_y[1] = true;
-                    if !taller_y[1] {
-                        score_y[1] += 1;
-                    }
-                }
+                if !taller_y[1] { score_y[1] += 1; }
+                if grid[y][j] >= grid[i][j] { taller_y[1] = true; }
             }
             let scenic_score = score_x[1] * score_x[0] * score_y[1] * score_y[0];
             if scenic_score > highest_scenic_score {highest_scenic_score = scenic_score;}
@@ -71,7 +48,6 @@ pub fn visible_trees(data_string: String) {
             }
         }
     }
-
     println!("visible trees: {visible_trees}");
     println!("highest scenic score: {}", highest_scenic_score);
 }
