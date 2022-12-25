74c7a0d66657cc1ac65c9e6e246602b6218ccda3
mhfvuong <mitchellvuong@hotmail.com>
2022-12-25 02:25:07 +0100

    day 12 part 1 is done

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,116 +1,111 @@
 use std::collections::HashMap;
 
+#[derive(Debug)]
+struct Graph {
+    map: Vec<Vec<u8>>,
+    nodes: Vec<usize>,
+    edges: HashMap<usize, Vec<usize>>,
+    queue: Vec<usize>,
+}
+
+impl Graph {
+    fn new() -> Self {
+        Graph {
+            map: vec![Vec::new()],
+            nodes: Vec::new(),
+            edges: HashMap::new(),
+            queue: Vec::new(),
+        }
+    }
+
+    fn add_node(&mut self, node: usize) {
+        self.nodes.push(node);
+        self.edges.insert(node, Vec::new());
+    }
+
+    fn add_edge(&mut self, node1: usize, height1: u8, node2: usize, height2: u8) {
+        if height1 + 1 >= height2 {
+            if let Some(edge) = self.edges.get_mut(&node1) {
+                edge.push(node2);
+            }
+        }
+    }
+
+    fn look_around_add(&mut self, h: usize, w: usize) {
+        let cn = w + h * self.map[0].len();
+        let width = self.map[0].len();
+        let ch = self.map[h][w];
+        if h > 0 {
+            self.add_edge(cn, ch, cn - width, self.map[h - 1][w]);
+        }
+        if h < self.map.len() - 1 {
+            self.add_edge(cn, ch, cn + width, self.map[h + 1][w]);
+        }
+        if w > 0 {
+            self.add_edge(cn, ch, cn - 1, self.map[h][w - 1]);
+        }
+        if w < width - 1 {
+            self.add_edge(cn, ch, cn + 1, self.map[h][w + 1]);
+        }
+    }
+}
+
 pub fn hill_climbing(data_string: String) {
     let lines = data_string.lines();
-    let mut start: [usize; 2] = [0; 2];
-    let mut end: [usize; 2] = [0; 2];
-    let mut map = vec![Vec::new()];
-    let mut path = vec![Vec::new()]; // debug path
-    let mut explored = Vec::new();
-    let mut queue = Vec::new();
-    let mut connections: HashMap<[usize; 2], Vec<[usize; 2]>> = HashMap::new();
-    for (i, line) in lines.enumerate(){
-        if i == map.len() {map.push(Vec::new())}
-        if i == path.len() {path.push(Vec::new())} // debug path
+    let mut graph = Graph::new();
+    let mut start: usize = 0;
+    let mut end: usize = 0;
+    let mut path_to = vec![Vec::new()];
+    for (i, line) in lines.enumerate() {
+        if i == graph.map.len() {
+            graph.map.push(Vec::new());
+            path_to.push(Vec::new());
+        }
+        let mut n = 0;
         for h in line.chars() {
-            connections.insert([i, map[i].len()], Vec::new());
-            if h == 'S' {start = [i, map[i].len()];}
-            else if h == 'E' { end = [i, map[i].len()];}
-            map[i].push(h);
-            path[i].push('.'); // debug path
-        }
-    }
-    // char.to_string().as_bytes()[0]
-    // check_map(&map,&path);
-    // println!("{:?}", &end);
-    for i in 0..map.len() {
-        for j in 0..map[0].len() {
-            let mut current = map[i][j];
-            match map[i][j] {
-                'S' => current = 'a',
-                'E' => current = 'z',
-                _ => current = map[i][j]
+            path_to[i].push(-1);
+            graph.add_node(n + i * graph.map[0].len());
+            if h == 'S' {
+                start = n + i * graph.map[0].len();
+                graph.map[i].push(String::from('a').as_bytes()[0]);
             }
-            compare_height(&mut connections, &map, current, i, j);
+            else if h == 'E' {
+                end = n + i * graph.map[0].len();
+                graph.map[i].push(String::from('z').as_bytes()[0]);
+            }
+            else {
+                graph.map[i].push(h.to_string().as_bytes()[0]);
+            }
+            n += 1;
         }
     }
-    // for c in connections.keys(){
-    //     println!("{:?} is connected to {:?}", &c, connections.get(c));
-    // }
-    queue.push(start);
-    while !queue.is_empty() {
-        // println!("queue: {:?}", &queue);
-        let current = queue.remove(0);
-        // add neighbors in queue
-            if current == end {
-                // explored.push(end);
-                queue.drain(..);
-            } else {
-                if let Some(neighbors) = connections.get(&current) {
-                    for n in neighbors {
-                        if !queue.contains(n) && !explored.contains(n) {
-                            // n[2] = current[2] + 1;
-                            queue.push(*n);
-                        }
+    for h in 0..graph.map.len() {
+        for w in 0..graph.map[0].len() {
+            graph.look_around_add(h, w);
+        }
+    }
+
+    path_to[start/graph.map[0].len()][start%graph.map[0].len()] = 0;
+    graph.queue.push(start);
+    while !graph.queue.is_empty() {
+        let current_node = graph.queue.remove(0);
+        let w = current_node % graph.map[0].len();
+        let h = current_node / graph.map[0].len();
+        let path = path_to[h][w] + 1;
+        if current_node != end {
+            if let Some(neighbors) = graph.edges.get(&current_node) {
+                for n_bors in neighbors {
+                    let hn = n_bors / graph.map[0].len();
+                    let wn = n_bors % graph.map[0].len();
+                    if path_to[hn][wn] == -1 || path < path_to[hn][wn] {
+                        path_to[hn][wn] = path;
+                        graph.queue.push(*n_bors);
                     }
                 }
-
-                explored.push(current);
-            }
-        // println!("Explored: {:?}", &explored);
-    }
-    println!("explored tiles until E has been found:");
-    println!("{:?}", explored);
-    println!("{}", explored.len());
-}
-
-fn check_map(map: &Vec<Vec<char>>, path_map: &Vec<Vec<char>>) {
-    println!("\n    map: ");
-    for i in map {
-        let mut map_line = String::new();
-        for j in i {
-            map_line.push(*j);
-        }
-        println!("{:?}", map_line);
-    }
-    println!("\n    Path map: ");
-    for i in path_map {
-        let mut pathmap_line = String::new();
-        for j in i {
-            pathmap_line.push(*j);
-        }
-        println!("{:?}", pathmap_line);
-    }
-
-}
-
-fn compare_height(connections: &mut HashMap<[usize; 2], Vec<[usize; 2]>>, map: &Vec<Vec<char>>, current: char, i: usize, j: usize) {
-    if i > 0 {
-        if current.to_string().as_bytes()[0] + 1 >= map[i - 1][j].to_string().as_bytes()[0] {
-            if let Some(new_connection) = connections.get_mut(&[i, j]) {
-                new_connection.push([i - 1, j]);
             }
         }
-    }
-    if i < map.len() - 1 {
-        if current.to_string().as_bytes()[0] + 1 >= map[i + 1][j].to_string().as_bytes()[0] {
-            if let Some(new_connection) = connections.get_mut(&[i, j]) {
-                new_connection.push([i + 1, j]);
-            }
-        }
-    }
-    if j > 0 {
-        if current.to_string().as_bytes()[0] + 1 >= map[i][j - 1].to_string().as_bytes()[0] {
-            if let Some(new_connection) = connections.get_mut(&[i, j]) {
-                new_connection.push([i, j - 1]);
-            }
-        }
-    }
-    if j < map[0].len() - 1 {
-        if current.to_string().as_bytes()[0] + 1 >= map[i][j + 1].to_string().as_bytes()[0] {
-            if let Some(new_connection) = connections.get_mut(&[i, j]) {
-                new_connection.push([i, j + 1]);
-            }
+        else {
+            println!("steps to reach the end: {}", path_to[h][w]);
         }
     }
 }
